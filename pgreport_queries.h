bf6049fdce33f205feea05f33d7938479c771700
Guillaume Lelarge <guillaume@lelarge.info>
2021-02-17 16:03:39 +0100

    Add a %usage of indexes

--- pgreport_queries.h
+++ pgreport_queries.h
@@ -102,6 +102,9 @@
 
 #define TOP10QUERIES_SQL "select queryid, query from pg_stat_statements order by total_time desc limit 10"
 
+#define PERCENTUSEDINDEXES_TITLE "Percentage usage of indexes"
+#define PERCENTUSEDINDEXES_SQL "SELECT relname, CASE idx_scan WHEN 0 THEN 'Insufficient data' ELSE (100 * idx_scan / (seq_scan + idx_scan))::text END percent_of_times_index_used, n_live_tup rows_in_table FROM pg_stat_user_tables ORDER BY n_live_tup DESC"
+
 #define UNUSEDINDEXES_TITLE "Unused indexes"
 #define UNUSEDINDEXES_SQL "select schemaname, count(*) from pg_stat_user_indexes s join pg_index i using (indexrelid) where idx_scan=0 and (not indisunique AND not indisprimary) group by 1;"
 
